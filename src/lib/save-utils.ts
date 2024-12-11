95759753320c7536cca4697be7189ade8d376098
iziz9 <46iz.us2@gmail.com>
2024-12-11 17:18:28 +0900

    Feat: 색상 저장 전 체크 기능 추가

--- src/lib/save-utils.ts
+++ src/lib/save-utils.ts
@@ -2,15 +2,29 @@ import { ISavedColors } from '@/constants/types'
 
 export const getSavedColors = () => {
   const savedColors = localStorage.getItem('saved-color')
-  return savedColors
+  return savedColors ? JSON.parse(savedColors) : null
 }
 
 export const saveColor = (colorInfo: ISavedColors) => {
   const savedColors = getSavedColors()
-  console.log(savedColors)
+  if (!savedColors) {
+    return localStorage.setItem('saved-color', JSON.stringify([colorInfo]))
+  }
 
-  localStorage.setItem(
-    'saved-color',
-    JSON.stringify(savedColors?.length ? [...JSON.parse(savedColors), colorInfo] : [colorInfo]),
-  )
+  const isSameColorSaved = checkSavedSameColor(colorInfo.hexCode)
+  if (isSameColorSaved) return alert('이미 저장된 색상입니다.')
+  // 아이콘 클릭 비활성화 필요
+
+  const newList = [...savedColors, colorInfo]
+  localStorage.setItem('saved-color', JSON.stringify(newList))
+  alert('색상이 저장되었습니다. \n마이페이지에서 저장된 색상을 확인할 수 있습니다.')
+  //alert 대신 아이콘 fill로 변경하기
+}
+
+export const checkSavedSameColor = (hexCode: string) => {
+  const savedColors = getSavedColors()
+  if (!savedColors) return false
+
+  const sameColor = savedColors.find((color: ISavedColors) => color.hexCode === hexCode)
+  return sameColor ? true : false
 }
