a5fa81051881261a3cac20ffcc251121cd5a9673
iziz9 <46iz.us2@gmail.com>
2024-12-23 23:27:14 +0900

    Feat: 크롭 영역 선택 기능 추가

--- src/components/editor/image-editor.tsx
+++ src/components/editor/image-editor.tsx
@@ -1,4 +1,5 @@
 'use client'
+import useImageCrop from '@/hooks/useImageCrop'
 import { CancelIcon, CropIcon, DeleteBinIcon, SaveIcon, UploadIcon } from '@/icons/editorIcons'
 import { CheckIcon } from '@/icons/icons'
 import { removeFormat } from '@/lib/file-utils'
@@ -6,30 +7,14 @@ import { ChangeEvent, useEffect, useRef, useState } from 'react'
 
 export default function ImageEditor() {
   const [file, setFile] = useState<File | null>(null)
-  const [onEditMode, setOnEditMode] = useState<boolean>(false)
   const fileInputRef = useRef<HTMLInputElement>(null)
-  const canvasRef = useRef<HTMLCanvasElement>(null)
   const downloadRef = useRef<HTMLAnchorElement>(null)
+  const { canvasRef, cropLayerRef, selectedAreaRef, onCropMode, setOnCropMode } = useImageCrop()
 
   useEffect(() => {
     drawImage()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [file])
-
-  useEffect(() => {
-    if (!onEditMode) return
-
-    const onEditKeyDownHandler = (event: KeyboardEvent) => {
-      if (event.key === 'Enter') console.log('적용하기')
-      if (event.key === 'Escape') setOnEditMode(false) //편집 취소
-    }
-
-    window.addEventListener('keydown', onEditKeyDownHandler)
-
-    return () => {
-      window.removeEventListener('keydown', onEditKeyDownHandler)
-    }
-  }, [onEditMode])
 
   const openFileSelectWindow = () => {
     fileInputRef.current?.click()
@@ -47,7 +32,7 @@ export default function ImageEditor() {
     const ctx = canvas?.getContext('2d')
     ctx?.clearRect(0, 0, canvas?.width || 0, canvas?.height || 0)
     setFile(null)
-    setOnEditMode(false)
+    setOnCropMode(false)
   }
 
   const drawImage = () => {
@@ -66,14 +51,7 @@ export default function ImageEditor() {
       canvas.height = img.height
       const ctx = canvas.getContext('2d')
       ctx?.drawImage(img, 0, 0, img.width, img.height) // 업로드한 이미지를 캔버스에 그리기(이미지객체, x좌표, y좌표)
-      URL.revokeObjectURL(imgUrl)
-
-      const dataURL = canvas.toDataURL('image/png')
-      if (downloadRef.current) {
-        downloadRef.current.download = `${removeFormat(file.name)}-edited.png`
-        downloadRef.current.href = dataURL
-        return URL.revokeObjectURL(imgUrl) // 캐싱된 이미지 URL 해제(메모리관리, 같은 url 중복 생성 및 다운로드 중복 방지)
-      }
+      URL.revokeObjectURL(imgUrl) // 캐싱된 이미지 URL 해제(메모리관리, 같은 url 중복 생성 및 다운로드 중복 방지)
     }
     img.onerror = () => {
       new Error('이미지를 로드하는 데 실패했습니다.')
@@ -82,13 +60,17 @@ export default function ImageEditor() {
   }
 
   const saveImg = () => {
-    if (!downloadRef.current) return
+    if (!downloadRef.current || !canvasRef.current) return
 
-    const fileName = prompt('저장할 파일명을 입력해주세요.') //모달로 변경하기
+    const fileName = prompt('저장할 파일명을 입력해주세요.') //모달로 변경하기, 파일 형식 선택창 추가
     if (!fileName) return
 
+    const canvas = canvasRef.current
+    const dataURL = canvas.toDataURL('image/png')
+
+    downloadRef.current.href = dataURL
     downloadRef.current.download = `${fileName}.png` // 다운로드 시 파일이름 설정
-    downloadRef.current?.click() //다운로드 링크 클릭이벤트 발생
+    downloadRef.current.click() //다운로드 링크 클릭이벤트 발생
   }
 
   return (
@@ -100,11 +82,11 @@ export default function ImageEditor() {
               <DeleteBinIcon />
             </button>
             <div className="flex gap-5">
-              {onEditMode ? (
+              {onCropMode ? (
                 <div className="flex gap-3">
                   <button
                     className="text-sm px-3 rounded-md flex gap-1 items-center"
-                    onClick={() => setOnEditMode(false)}
+                    onClick={() => setOnCropMode(false)}
                   >
                     <CancelIcon />
                     <span>편집 취소</span>
@@ -112,7 +94,7 @@ export default function ImageEditor() {
                   <button
                     className="text-sm px-3 rounded-md flex gap-1 items-center"
                     onClick={() => {
-                      setOnEditMode(false)
+                      setOnCropMode(false)
                       console.log('자르기')
                     }}
                   >
@@ -121,21 +103,27 @@ export default function ImageEditor() {
                   </button>
                 </div>
               ) : (
-                <button onClick={() => setOnEditMode(true)}>
-                  <CropIcon />
-                </button>
+                <>
+                  <button
+                    onClick={() => {
+                      setOnCropMode(true)
+                    }}
+                  >
+                    <CropIcon />
+                  </button>
+                  <button onClick={saveImg}>
+                    <SaveIcon />
+                  </button>
+                  <a href="" ref={downloadRef} className="hidden"></a>
+                </>
               )}
-              {/* <button onClick={() => console.log('이미지 효과')}>
-                <EffectIcon />
-              </button> */}
-              <button onClick={saveImg}>
-                <SaveIcon />
-              </button>
-              <a href="" ref={downloadRef} className="hidden"></a>
             </div>
           </section>
-          <section className="w-full border-2 border-[#e5e7eb] bg-[#e5e7eb] p-5">
+          <section className="relative w-full border-2 border-[#e5e7eb] bg-[#e5e7eb] p-5">
             <canvas ref={canvasRef} className="max-w-[80%] max-h-[550px] m-auto"></canvas>
+            {onCropMode && (
+              <canvas ref={cropLayerRef} className="absolute inset-0 max-w-[80%] max-h-[550px] m-auto"></canvas>
+            )}
           </section>
         </div>
       ) : (
---
+++ src/hooks/useImageCrop.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useRef, useState } from 'react'
+
+const MIN_SIZE = 50
+
+const useImageCrop = () => {
+  const [onCropMode, setOnCropMode] = useState<boolean>(false)
+  const [isDrawing, setIsDrawing] = useState(false) // 크롭 영역 선택중인지 판별
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const cropLayerRef = useRef<HTMLCanvasElement>(null)
+  const selectedAreaRef = useRef({ sX: 0, sY: 0, eX: 0, eY: 0 }) // 선택영역의 시작 및 끝 좌표값
+
+  useEffect(() => {
+    if (!onCropMode || !cropLayerRef.current || !canvasRef.current) return
+
+    const cropLayer = cropLayerRef.current
+    cropLayer.width = canvasRef.current.width
+    cropLayer.height = canvasRef.current.height
+
+    window.addEventListener('keydown', onEditKeyDownHandler)
+    cropLayer.addEventListener('mousedown', onCropStartHandler) //마우스를 누르기 시작할 때
+    cropLayer.addEventListener('mouseup', onCropEndHandler)
+
+    return () => {
+      window.removeEventListener('keydown', onEditKeyDownHandler)
+      cropLayer.removeEventListener('mousedown', onCropStartHandler)
+      cropLayer.removeEventListener('mouseup', onCropEndHandler)
+    }
+  }, [onCropMode])
+
+  useEffect(() => {
+    if (!isDrawing || !cropLayerRef.current) return
+
+    const cropLayer = cropLayerRef.current
+
+    cropLayer.addEventListener('mousemove', onCropAreaHandler) // 마우스를 누르고 끌 때
+    return () => {
+      cropLayer.removeEventListener('mousemove', onCropAreaHandler)
+    }
+  }, [isDrawing])
+
+  const onEditKeyDownHandler = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') console.log('적용하기')
+    if (event.key === 'Escape') setOnCropMode(false) //편집 취소
+  }
+
+  const onCropStartHandler = (e: MouseEvent) => {
+    if (!cropLayerRef.current) return
+
+    const cropLayer = cropLayerRef.current
+    const selectedArea = selectedAreaRef.current
+
+    const canvasX = cropLayer.getBoundingClientRect().left // 캔버스의 시작점 - 뷰포트에서의 x좌표
+    const canvasY = cropLayer.getBoundingClientRect().top // 캔버스의 시작점 - 뷰포트에서의 y좌표
+
+    const sX = e.clientX - canvasX // 캔버스 안에서의 x좌표
+    const sY = e.clientY - canvasY // 캔버스 안에서의 y좌표
+
+    selectedArea.sX = sX
+    selectedArea.sY = sY
+    setIsDrawing(true)
+  }
+
+  const onCropAreaHandler = (e: MouseEvent) => {
+    if (!isDrawing || !cropLayerRef.current || !canvasRef.current) return
+
+    const cropLayer = cropLayerRef.current
+    const selectedArea = selectedAreaRef.current
+
+    const canvasX = cropLayer.getBoundingClientRect().left // 캔버스의 시작점 - 뷰포트에서의 x좌표
+    const canvasY = cropLayer.getBoundingClientRect().top // 캔버스의 시작점 - 뷰포트에서의 y좌표
+
+    const eX = e.clientX - canvasX
+    const eY = e.clientY - canvasY
+
+    const selectedEndX = eX - selectedArea.sX // 마우스를 이동한 x거리 (넓이)
+    const selectedEndY = eY - selectedArea.sY // 마우스를 이동한 y거리 (높이)
+
+    const ctx = cropLayer.getContext('2d')
+    ctx?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height) // 다시 그릴 때 캔버스 초기화
+    ctx?.strokeRect(selectedArea.sX, selectedArea.sY, selectedEndX, selectedEndY) // 마우스를 이동한 좌표 영역만큼 사각형 그리기(시작x좌표, 시작y좌표, 넓이, 높이)
+
+    selectedArea.eX = eX // 캔버스 내 좌표
+    selectedArea.eY = eY // 캔버스 내 좌표
+  }
+
+  const onCropEndHandler = () => {
+    setIsDrawing(false)
+    drawCroppedImg()
+  }
+
+  const drawCroppedImg = () => {
+    if (!cropLayerRef.current) return
+    console.log(selectedAreaRef.current)
+
+    const img = new Image()
+    // const imgUrl = URL.createObjectURL('')//
+    // img.src = imgUrl
+
+    const cropLayer = cropLayerRef.current
+    if (!cropLayer) return new Error('canvas를 사용할 수 없습니다.')
+  }
+
+  return { canvasRef, cropLayerRef, selectedAreaRef, onCropMode, setOnCropMode }
+}
+
+export default useImageCrop
